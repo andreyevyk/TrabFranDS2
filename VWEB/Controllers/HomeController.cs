ff4059f689becf3a7e035ece038a240b496e8d22
Andrey Evyk <andreyevyk13@gmail.com>
2019-05-20 23:57:25 -0300

    Adicionar arquivos de projeto.

---
+++ VWEB/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VWEB.Models;
+
+namespace VWEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private VWEBContext db = new VWEBContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                bool toGo = false;
+                foreach (var item in db.Usuarios.ToList())
+                {
+                    if (usuario.Email == item.Email)
+                    {
+                        if (usuario.Senha == item.Senha)
+                        {
+                            toGo = true;
+                            break;
+                        }
+                    }
+
+                }
+                if (toGo)
+                {
+                    return RedirectToAction("Index", "Alunos");
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ VWEB/Models/VWEBContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace VWEB.Models
+{
+    public class VWEBContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public VWEBContext() : base("name=VWEBContext")
+        {
+        }
+
+      public System.Data.Entity.DbSet<VWEB.Models.Aluno> Alunos { get; set; }
+
+      public System.Data.Entity.DbSet<VWEB.Models.Responsavel> Responsavels { get; set; }
+
+      public System.Data.Entity.DbSet<VWEB.Models.Turma> Turmas { get; set; }
+
+        public System.Data.Entity.DbSet<VWEB.Models.Usuario> Usuarios { get; set; }
+    }
+}
